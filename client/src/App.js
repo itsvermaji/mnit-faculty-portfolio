f11533d2734a4488894fcc0aff96fc6cc7117da9
Renon133 <balkrishan.sehra03@gmail.com>
2021-11-22 00:28:51 +0530

    screenshot added

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
 import LoginForm from './Components/Login/LoginForm';
-<<<<<<< HEAD
+import Main_page from './Components/HomePage/Main_page';
 
 function App() {
   return (
     <div>
-=======
-import Main_page from './Components/HomePage/Main_page';
-function App() {
-  return (
-    <div>
       <Main_page/>
->>>>>>> a997d5085c3092f60a788a7678955b44f31eeb8d
       <LoginForm email="balkrishan@123" password="password"/>
     </div>
   );
